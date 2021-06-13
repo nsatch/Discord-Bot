e6128e0958f688fb4bc43225eaf51669567862e8
nsatch <email@email.com>
2021-06-13 15:04:04 -0500

    Removed test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-nameQuote = "Hello there this is a test"
-split = nameQuote.split(' ', 1)    #Split the string by white space, but only do once (so we have [name, quote])
-name = split[0]
-quote = split[1]
-print(len(split))
-print(name)
-print(quote)
